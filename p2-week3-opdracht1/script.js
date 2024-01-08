7948c0eb81947bdc4cce0bdc14cc8799c8142d86
revskovic <9021911@student.tcrmbo.nl>
2024-01-08 14:10:18 +0100

    p2 week 3 opdracht 1

---
+++ p2-week3-opdracht1/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var winkelmandjeInhoud = ["xbox", "Playstation", "pc"];
+
+    var winkelmandjeDiv = document.getElementById("winkelmandje");
+    var aantalProductenDiv = document.getElementById("aantalProducten");
+
+    // Toon het aantal producten boven de lijst
+    aantalProductenDiv.textContent = "Aantal producten in het mandje: " + winkelmandjeInhoud.length;
+
+    for (var i = 0; i < winkelmandjeInhoud.length; i++) {
+        var productDiv = document.createElement("div");
+        productDiv.className = "product";
+        productDiv.textContent = winkelmandjeInhoud[i];
+        winkelmandjeDiv.appendChild(productDiv);
+    }
+});
